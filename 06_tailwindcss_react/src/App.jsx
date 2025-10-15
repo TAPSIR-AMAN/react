3fa8e3999b552164d1b5057dbd16c1fdca2759a7
TAPSIR-AMAN <tapsiraman477@gmail.com>
2025-10-16 02:46:57 +0530

    06_talwind

---
+++ 06_tailwindcss_react/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-500'>
+      <h1 className=' lg:bg-yellow-300 text-3xl font-bold underline sm:bg-red-600 md:bg-green-500 xl:bg-emerald-950 bg-orange-500' >hello</h1>
+    </div>
+  )
+}
+
+export default App
