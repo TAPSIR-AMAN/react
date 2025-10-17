2654c73e7baf38b47d0c97b286a9f4ef646057e2
TAPSIR-AMAN <tapsiraman477@gmail.com>
2025-10-17 17:12:22 +0530

    08_function

---
+++ 08_function_react/src/App.jsx
@@ -0,0 +1,62 @@
+// import React from 'react'
+
+// const App = () => {
+//   // const btn=()=>{
+//   //   console.log("click on me")
+//   // }
+//   const change=(elem)=>{
+//     console.log("change is "+elem);
+    
+//   }
+//   const pagescroll=(elem)=>{
+//    if (elem>0) {
+//     console.log("seeda scrolling");
+    
+//    }else{
+//     console.log("ulta scrolling");
+    
+//    }
+    
+//   }
+//   return (
+
+//     <div onWheel={(elem)=>{
+//       pagescroll(elem.deltaY)
+//     }}>
+//       {/* <h1>hello</h1>
+//       <button  onClick={btn}>click me</button>
+//       <button onClick={function(){
+//         console.log("dont click on me")
+//       }}>this is new</button> */}
+// {/* 
+//       <input onChange={function(elem){
+//         change(elem.target.value)
+        
+//       }} type="text" name="" id=" " placeholder='Enter ur name' /> */}
+
+//       {/* <div onMouseMove={(elem)=>{
+//         console.log(elem.pageX);
+        
+//       }} className="box"></div> */}
+
+//       {/* <div className="page1"></div>
+//       <div className="page2"></div>
+//       <div className="page3"></div> */}
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+export default App
